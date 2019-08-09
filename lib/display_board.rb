50d94852693dd5209e0f2aa50c1c280eb9eca323
yaoyu bao <yaobao5354@gmail.com>
2019-08-09 06:10:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board 
-row_odd = "   "  
-  
+row_odd = "   " + "|" + "   " + "|" + "   "
+row_even = "-----------"
+put
 end
